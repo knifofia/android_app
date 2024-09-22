7491d90f1c23c1495066d3133f5f16ff6651730c
knifofia <guillaume.crand2000@gmail.com>
2024-09-22 20:17:00 +0200

    initial commit

---
+++ app/src/main/java/knifofia/menu/MainActivity.kt
@@ -0,0 +1,34 @@
+package knifofia.menu
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random.nextInt(seekBar.progress)+1
+            resultsTextView.text = rand.toString()
+        }
+    }
+}
